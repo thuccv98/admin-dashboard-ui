c614f4de3385bd535b500eea7727132c8e8a77a8
thuccv98 <thuccv98@gmail.com>
2021-09-17 17:07:54 +0700

    Create chart component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,18 @@
+import Chart from '../../components/chart/Chart';
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import { userData } from '../../datas';
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
